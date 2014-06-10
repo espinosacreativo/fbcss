b7258e02988f498833f696f2578dc7b9f791ae23
espinosacreativo <nuke@MacBook-Pro-de-nuke.local>
2014-06-10 22:13:13 +0200

    Test3
    test

--- ico.js
+++ ico.js
@@ -1,2 +1,2 @@
     	addIcono("ico1","https://dl.dropboxusercontent.com/s/5v51oh9lr20wiw4/Emoji%20Smiley-01.png");
-        addIcono("ico2","http://findicons.com/files/icons/360/emoticons/128/happy.png");
+        addIcono("ico2","http://findicons.com/files/icons/360/emoticons/128/happy.png"); 
