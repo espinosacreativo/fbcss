76f0649e7e03113073860ba4eeffd07ff29170fd
espinosacreativo <espinosacreativo@gmail.com>
2014-06-10 22:32:38 +0200

    Update ico.js

--- ico.js
+++ ico.js
@@ -2,5 +2,32 @@
    		document.getElementById("iconos").innerHTML+="<img id='"+id+"' href='javascript:void(0);' width='30px' onclick='document.getElementById(\"vB_Editor_001_textarea\").value+=\"[img]"+src+"[/img]\";' class='iconos' src='"+src+"'>";
    	}
      
-    	addIcono("ico1","https://dl.dropboxusercontent.com/s/5v51oh9lr20wiw4/Emoji%20Smiley-01.png");
-        addIcono("ico2","http://findicons.com/files/icons/360/emoticons/128/happy.png"); 
+addIcono("ico1","http://goo.gl/yUBL5N");;
+addIcono("ico2","http://goo.gl/pN309h");
+addIcono("ico3","http://goo.gl/PYIgdk");
+addIcono("ico4","http://goo.gl/WlEfxR");
+addIcono("ico5","http://goo.gl/BLMKzn");
+addIcono("ico6","http://goo.gl/uZ3WBI");
+addIcono("ico7","http://goo.gl/5442st");
+addIcono("ico8","http://goo.gl/d7F1Ml");
+addIcono("ico9","http://goo.gl/FM1x1v");
+addIcono("ico10","http://goo.gl/LFSmJ1");
+addIcono("ico11","http://goo.gl/sfl34P");
+addIcono("ico12","http://goo.gl/tBxAhD");
+addIcono("ico13","http://goo.gl/8mk5XS");
+addIcono("ico14","http://goo.gl/yjQKnE");
+addIcono("ico15","http://goo.gl/EJxQhm");
+addIcono("ico16","http://goo.gl/IKzm9I");
+addIcono("ico17","http://goo.gl/YQcmpF");
+addIcono("ico18","http://goo.gl/uJ0j7W");
+addIcono("ico19","http://goo.gl/CO0XiD");
+addIcono("ico20","http://goo.gl/09SWh0");
+addIcono("ico21","http://goo.gl/Cf1N7U");
+addIcono("ico22","http://goo.gl/WzxQ8h");
+addIcono("ico23","http://goo.gl/0oWFU0");
+addIcono("ico24","http://goo.gl/3eqFwr");
+addIcono("ico25","http://goo.gl/gxNqu4");
+addIcono("ico26","http://goo.gl/MsIVuZ");
+addIcono("ico27","http://goo.gl/LH7qWg");
+addIcono("ico28","http://goo.gl/PyiQZQ");
+addIcono("ico29","http://goo.gl/wtfht4");
