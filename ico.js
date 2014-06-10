ac25a11f2c5234561886eb89be5b1828fbc79e03
espinosacreativo <espinosacreativo@gmail.com>
2014-06-10 22:48:20 +0200

    Update ico.js

--- ico.js
+++ ico.js
@@ -1,33 +1,9 @@
-       	function addIcono(id,src){
-   		document.getElementById("iconos").innerHTML+="<img id='"+id+"' href='javascript:void(0);' width='30px' onclick='document.getElementById(\"vB_Editor_001_textarea\").value+=\"[img]"+src+"[/img]\";' class='iconos' src='"+src+"'>";
+       	function addIcono(id,drop,src){
+   		document.getElementById("iconos").innerHTML+="<img id='"+id+"' href='javascript:void(0);' width='30px' onclick='document.getElementById(\"vB_Editor_001_textarea\").value+=\"[img]"+src+"[/img]\";' class='iconos' src='"+drop+"'>";
    	}
-     
-addIcono("ico1","http://goo.gl/yUBL5N");;
-addIcono("ico2","http://goo.gl/pN309h");
-addIcono("ico3","http://goo.gl/PYIgdk");
-addIcono("ico4","http://goo.gl/WlEfxR");
-addIcono("ico5","http://goo.gl/BLMKzn");
-addIcono("ico6","http://goo.gl/uZ3WBI");
-addIcono("ico7","http://goo.gl/5442st");
-addIcono("ico8","http://goo.gl/d7F1Ml");
-addIcono("ico9","http://goo.gl/FM1x1v");
-addIcono("ico10","http://goo.gl/LFSmJ1");
-addIcono("ico11","http://goo.gl/sfl34P");
-addIcono("ico12","http://goo.gl/tBxAhD");
-addIcono("ico13","http://goo.gl/8mk5XS");
-addIcono("ico14","http://goo.gl/yjQKnE");
-addIcono("ico15","http://goo.gl/EJxQhm");
-addIcono("ico16","http://goo.gl/IKzm9I");
-addIcono("ico17","http://goo.gl/YQcmpF");
-addIcono("ico18","http://goo.gl/uJ0j7W");
-addIcono("ico19","http://goo.gl/CO0XiD");
-addIcono("ico20","http://goo.gl/09SWh0");
-addIcono("ico21","http://goo.gl/Cf1N7U");
-addIcono("ico22","http://goo.gl/WzxQ8h");
-addIcono("ico23","http://goo.gl/0oWFU0");
-addIcono("ico24","http://goo.gl/3eqFwr");
-addIcono("ico25","http://goo.gl/gxNqu4");
-addIcono("ico26","http://goo.gl/MsIVuZ");
-addIcono("ico27","http://goo.gl/LH7qWg");
-addIcono("ico28","http://goo.gl/PyiQZQ");
-addIcono("ico29","http://goo.gl/wtfht4");
+("ico1","https://www.dropbox.com/s/lrv3ryjjikjeg53/Emoji%20Smiley-01.png","http://goo.gl/yUBL5N")
+("ico2","https://www.dropbox.com/s/1hs5cof2g95pp1b/Emoji%20Smiley-02.png","http://goo.gl/pN309h")
+("ico3","https://www.dropbox.com/s/vangqlxemcea8nd/Emoji%20Smiley-03.png","http://goo.gl/PYIgdk")
+("ico4","https://www.dropbox.com/s/20gfruk85u5bnos/Emoji%20Smiley-04.png","http://goo.gl/WlEfxR")
+("ico5","https://www.dropbox.com/s/1noxju8op8gr5sd/Emoji%20Smiley-05.png","http://goo.gl/BLMKzn")
+("ico6","https://www.dropbox.com/s/q2byqnb0dvbwfpf/Emoji%20Smiley-06.png","http://goo.gl/uZ3WBI")
