199d5269ccdd114fce0277e3856304725a5afff4
espinosacreativo <espinosacreativo@gmail.com>
2014-06-10 22:49:26 +0200

    Update ico.js

--- ico.js
+++ ico.js
@@ -1,9 +1,9 @@
        	function addIcono(id,drop,src){
    		document.getElementById("iconos").innerHTML+="<img id='"+id+"' href='javascript:void(0);' width='30px' onclick='document.getElementById(\"vB_Editor_001_textarea\").value+=\"[img]"+src+"[/img]\";' class='iconos' src='"+drop+"'>";
    	}
-("ico1","https://www.dropbox.com/s/lrv3ryjjikjeg53/Emoji%20Smiley-01.png","http://goo.gl/yUBL5N")
-("ico2","https://www.dropbox.com/s/1hs5cof2g95pp1b/Emoji%20Smiley-02.png","http://goo.gl/pN309h")
-("ico3","https://www.dropbox.com/s/vangqlxemcea8nd/Emoji%20Smiley-03.png","http://goo.gl/PYIgdk")
-("ico4","https://www.dropbox.com/s/20gfruk85u5bnos/Emoji%20Smiley-04.png","http://goo.gl/WlEfxR")
-("ico5","https://www.dropbox.com/s/1noxju8op8gr5sd/Emoji%20Smiley-05.png","http://goo.gl/BLMKzn")
-("ico6","https://www.dropbox.com/s/q2byqnb0dvbwfpf/Emoji%20Smiley-06.png","http://goo.gl/uZ3WBI")
+addIcono("ico1","https://www.dropbox.com/s/lrv3ryjjikjeg53/Emoji%20Smiley-01.png","http://goo.gl/yUBL5N")
+addIcono("ico2","https://www.dropbox.com/s/1hs5cof2g95pp1b/Emoji%20Smiley-02.png","http://goo.gl/pN309h")
+addIcono("ico3","https://www.dropbox.com/s/vangqlxemcea8nd/Emoji%20Smiley-03.png","http://goo.gl/PYIgdk")
+addIcono("ico4","https://www.dropbox.com/s/20gfruk85u5bnos/Emoji%20Smiley-04.png","http://goo.gl/WlEfxR")
+addIcono("ico5","https://www.dropbox.com/s/1noxju8op8gr5sd/Emoji%20Smiley-05.png","http://goo.gl/BLMKzn")
+addIcono("ico6","https://www.dropbox.com/s/q2byqnb0dvbwfpf/Emoji%20Smiley-06.png","http://goo.gl/uZ3WBI")
