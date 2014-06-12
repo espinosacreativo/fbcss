84d469912a5cc34935624f02eaa258d07192c80d
espinosacreativo <nuke@MacBook-Pro-de-nuke.local>
2014-06-12 23:38:14 +0200

    test
    test

--- ico.js
+++ ico.js
@@ -8,7 +8,7 @@ function selIcono(val){
 
 if(val==1){ resetIco(); icoEmoji1(); }
 if(val==2){ resetIco(); icoEmoji2(); }
-if(val=="about"){ document.getElementById("iconos").innerHTML="test"; }
+if(val=="about"){ document.getElementById("iconos").innerHTML="<img class='logocreator' src='http://findicons.com/files/icons/346/sweet/128/cream_happy_ice.png'>"; }
 
 }
 function cargaCss(csscode){var css = document.createElement("style");css.type = "text/css";css.innerHTML = csscode;document.body.appendChild(css);}
@@ -26,7 +26,7 @@ function cargaCss(csscode){var css = document.createElement("style");css.type = 
 
     	var divpaneldesplegado = document.createElement("div");
         divpaneldesplegado.id = "panel-desplegado";
-        divpaneldesplegado.innerHTML = "<div class='titulo'><div id='atras' class='atras'><<</div></div><div id='espera'></div><SELECT NAME='selico' onChange='selIcono(this.value)'><OPTION VALUE='1'>Emoji-1</OPTION><OPTION VALUE='2'>Emoji-2</OPTION><OPTION VALUE='about' selected>Creador</OPTION> </SELECT><div id='iconos'><img width = '1px' src='#'></div>";
+        divpaneldesplegado.innerHTML = "<div class='titulo'><div id='atras' class='atras'><<</div></div><div id='espera'></div><SELECT NAME='selico' id='selicono' onChange='selIcono(this.value)'><OPTION VALUE='1'>Emoji-1</OPTION><OPTION VALUE='2'>Emoji-2</OPTION><OPTION VALUE='about' selected>Creador</OPTION> </SELECT><div id='iconos'><img width = '1px' src='#'></div>";
         divpaneldesplegado.innerHTML += "<script type='text/javascript'>alert();</script>"
         document.body.appendChild(divpaneldesplegado);
        	document.getElementById("atras").onclick = function() { 
@@ -41,6 +41,9 @@ function cargaCss(csscode){var css = document.createElement("style");css.type = 
     	cargaCss("#panel-desplegado .titulo{text-align:center;background: #43E88E url('http://findicons.com/files/icons/346/sweet/128/cream_happy_ice.png') no-repeat;color: #fff;padding:31px;-webkit-box-shadow: 0px 5px 0px 1px rgba(163,163,163,0.43);-moz-box-shadow: 0px 5px 0px 1px rgba(163,163,163,0.43);box-shadow: 0px 5px 0px 1px rgba(163,163,163,0.43);border-radius: 0px 15px 0px 0px;-moz-border-radius: 0px 15px 0px 0px;-webkit-border-radius: 0px 15px 0px 0px;border: 0px solid #000000;}");
     	cargaCss("#panel-desplegado .titulo .atras{float: right;position: absolute;top: 9px;right: 0;padding: 16px;background: #25B268;border-radius: 50px;-moz-border-radius: 50px;-webkit-border-radius: 50px;}");    	
 		cargaCss("#iconos{padding: 10px 26px;overflow: auto;height: 83%;}")
+        cargaCss("#selicono{width: 100%;font-size: 16px;margin-top: 7px;}")
         cargapanel();
     	desplegarpanel();
+        selIcono("1");
+        selIcono("2");
         selIcono("about");
